655341a0fde40ecfb28072fad9fc198ea433a258
Lucas Matheus Nunes Pereira <Lucasmathues416@outlook.com>
2022-05-13 18:00:56 -0300

    Alterações js, agr pode deletar tarefas!

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,16 @@ var textoAtividade = document.getElementById("texto");
 var button = document.getElementById("button");
 var cPai = document.getElementById("lista-itens");
 
+function deletaTarefa(divId) {
+  let div = document.getElementById(divId);
+  div.remove();
+  console.log(`div: ${divId} deletado!`);
+}
+
 function checkCheck(id, idLabel) {
-  idCheck = document.getElementById(id);
-  Label = document.getElementById(idLabel);
+  let idCheck = document.getElementById(id);
+  let Label = document.getElementById(idLabel);
+
   if (idCheck.checked) {
     Label.style.textDecoration = "line-through";
     Label.style.color = "gray";
@@ -20,23 +27,33 @@ function criaElemento(textoAtividade) {
   if (textoAtividade == "") {
     return;
   }
+
   var codigo = Math.random();
+  var codigoDiv = Math.random();
   var codigoLabel = Math.random();
+
   var novoDiv = document.createElement("div");
-  novoDiv.className = "items";
+  novoDiv.className = `items`;
+  novoDiv.id = `${codigoDiv}`;
+  console.log(`div: ${novoDiv.id}, criado!`);
 
   var novoLi = document.createElement("label");
   novoLi.className = `item-descricao ${codigo}`;
   novoLi.id = codigoLabel;
-  novoLi.setAttribute("for", `${codigo}`);
+  console.log(`Li: ${novoLi.id}, criado!`);
 
   var novoInput = document.createElement("input");
   novoInput.type = "checkbox";
   novoInput.className = `check ${codigo}`;
   novoInput.id = `${codigo}`;
+  novoInput.draggable = "true";
   novoInput.onclick = function () {
     checkCheck(codigo, codigoLabel);
   };
+  novoInput.ondragend = function () {
+    deletaTarefa(novoDiv.id);
+  };
+  console.log(`novo Input: ${novoInput.id}, criado!`);
 
   var novoTexto = document.createTextNode(textoAtividade);
 
