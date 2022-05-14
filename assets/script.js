32d5ffc1de286668549b3b816f7432aee356fac5
Lucas Matheus Nunes Pereira <Lucasmathues416@outlook.com>
2022-05-14 15:52:30 -0300

    trocar cor do input ao deletar

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,12 @@ var textoAtividade = document.getElementById("texto");
 var button = document.getElementById("button");
 var cPai = document.getElementById("lista-itens");
 
+function trocaCorInput(inputId) {
+  let input = document.getElementById(inputId);
+  input.style.setProperty("border", "0.15rem solid red");
+  input.style.setProperty("color", "red");
+}
+
 function deletaTarefa(divId) {
   let div = document.getElementById(divId);
   div.remove();
@@ -13,12 +19,13 @@ function checkCheck(id, idLabel) {
   let Label = document.getElementById(idLabel);
 
   if (idCheck.checked) {
-    Label.style.textDecoration = "line-through";
-    Label.style.color = "gray";
+    Label.style.setProperty("textDecoration", "line-through");
+    Label.style.setProperty("color", "gray");
   }
 
   if (!idCheck.checked) {
-    Label.style.textDecoration = "none";
+    Label.style.setProperty("textDecoration", "none");
+    Label.style.setProperty("color", "none");
     Label.style.color = "black";
   }
 }
@@ -33,26 +40,31 @@ function criaElemento(textoAtividade) {
   var codigoLabel = Math.random();
 
   var novoDiv = document.createElement("div");
-  novoDiv.className = `items`;
-  novoDiv.id = `${codigoDiv}`;
+  novoDiv.setAttribute("class", "items");
+  novoDiv.setAttribute("id", `${codigoDiv}`);
   console.log(`div: ${novoDiv.id}, criado!`);
 
   var novoLi = document.createElement("label");
-  novoLi.className = `item-descricao ${codigo}`;
-  novoLi.id = codigoLabel;
+  novoLi.setAttribute("class", `item-descricao ${codigo}`);
+  novoLi.setAttribute("id", codigoLabel);
   console.log(`Li: ${novoLi.id}, criado!`);
 
   var novoInput = document.createElement("input");
-  novoInput.type = "checkbox";
-  novoInput.className = `check ${codigo}`;
-  novoInput.id = `${codigo}`;
-  novoInput.draggable = "true";
+  novoInput.setAttribute("type", "checkbox");
+  novoInput.setAttribute("class", `check ${codigo}`);
+  novoInput.setAttribute("id", codigo);
+  novoInput.setAttribute("draggable", "true");
+
   novoInput.onclick = function () {
     checkCheck(codigo, codigoLabel);
+  };
+  novoInput.ondragstart = function () {
+    trocaCorInput(novoInput.id);
   };
   novoInput.ondragend = function () {
     deletaTarefa(novoDiv.id);
   };
+
   console.log(`novo Input: ${novoInput.id}, criado!`);
 
   var novoTexto = document.createTextNode(textoAtividade);
